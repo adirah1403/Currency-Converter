009c9b5c2782dea4b5b97c6095caa1958f9b8a04
adirah1403 <aheraditya2004@gmail.com>
2024-07-13 08:15:39 +0530

    Uploading Files

---
+++ currency.js
@@ -0,0 +1,59 @@
+const EXCG_URL = "https://2024-07-09.currency-api.pages.dev/v1/currencies/";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const btn = document.querySelector("form button");
+
+
+for (let select of dropdowns) {
+    for (currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name == "from" && currCode == "USD"){
+            newOption.selected = "selected";
+        } else if (select.name == "to" && currCode == "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateRate = async () =>{
+    let amt = document.querySelector(".amt input");
+    let amtval = amt.value;
+    if (amtval == "" || amtval<1){
+        amtval = 1;
+        amt.value = "1";
+    }
+    
+    const URL = `${EXCG_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    
+    let finalAmount = amtval*rate;
+    msg.innerText = `${amtval} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc =`https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateRate();
+});
+
+window.addEventListener("load", () => {
+    updateRate();
+});
